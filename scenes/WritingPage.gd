384a34d26fb703a690472004bc83bbc21904e5a9
glyph <mail@strangegly.ph>
2024-11-06 18:04:16 +0100

    Randomize text

--- scenes/WritingPage.gd
+++ scenes/WritingPage.gd
@@ -6,10 +6,22 @@ var next_rune = 0
 
 var _tweens = []
 
+
+const _TEXT = "Thou who openeth this tome, we beg of thee: Desist. Naught but pain and sorrow "\
+	+ "awaiteth thee, dear reader; neither gain nor honor are bound within these pages. "\
+	+ "Heed this warning of us who were considered wise and powerful: What is written here "\
+	+ "is dangerous and repulsive."
+var _text_idx = 0
+var _text_buffer = 0
+
+export(Array, SpriteFrames) var letter_runes = []
+export(SpriteFrames) var terminator_rune = load("img/runes/urbit_anim/urbit_anim.tres")
+
 signal rune_drawn
 signal page_filled
 
 func _ready():
+	_text_idx = rand_range(0, _TEXT.length())
 	reinit_runes()
 	init_tweens()
 
@@ -30,6 +42,20 @@ func _reinit_rune(idx):
 	rune.playing = false
 	rune.frame = 0
 	
+	if _text_buffer == 0:
+		rune.frames = terminator_rune
+		for i in range(0, 4):
+			var next_byte = _TEXT.ord_at(_text_idx)
+			_text_idx += 1
+			if _text_idx == _TEXT.length():
+				_text_idx = 0
+				
+			_text_buffer += next_byte << (8 * i)
+	else:
+		var next_rune_shape = letter_runes[_text_buffer % letter_runes.size()]
+		_text_buffer /= letter_runes.size()
+		rune.frames = next_rune_shape
+	
 func init_tweens():
 	for i in range(0, _num_runes):
 		_tweens.push_back(get_tree().create_tween())
@@ -44,7 +70,7 @@ func _init_tween(idx):
 	tween.tween_property(rune, "modulate", Color.white, .125)\
 		.set_trans(Tween.TRANS_LINEAR)\
 		.set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(rune, "modulate", Globals.COLOR_INK_ACTIVE_TRANS, 1)\
+	tween.tween_property(rune, "modulate", Globals.COLOR_INK_ACTIVE_TRANS, .25)\
 		.set_trans(Tween.TRANS_EXPO)\
 		.set_ease(Tween.EASE_OUT)
 	tween.tween_callback(self, "_reinit_rune", [idx])
